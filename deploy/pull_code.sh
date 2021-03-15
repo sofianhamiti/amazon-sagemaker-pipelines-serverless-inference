73547714cf56403e564dcd0bab7e7fc9c96a785c
SofianHamiti <sofian.hamiti@gmail.com>
2021-03-15 14:57:17 +0000

    initial commit

--- deploy/pull_code.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-security delete-internet-password -s "git-codecommit.eu-west-1.amazonaws.com" ~/Library/Keychains/login.keychain-db
-git pull
--- deploy/push_code.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-find . -name .DS_Store -print0 | xargs -0 git rm --ignore-unmatch
-security delete-internet-password -s "git-codecommit.eu-west-1.amazonaws.com" ~/Library/Keychains/login.keychain-db
-git add .
-git commit -m "commit-"$(date +%s)
-git push
